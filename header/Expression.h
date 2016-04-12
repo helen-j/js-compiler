01190d8987d393c892dd06e347fd82b14ea7b9ae
Jack Li <jackli900421@hotmail.com>
2016-04-12 19:48:05 +1000

    Defined virtual methods in each class for future testing.

--- header/Expression.h
+++ header/Expression.h
@@ -1,30 +1,43 @@
 #pragma once
+#include <iostream>
 #include "Node.h"
+using namespace std;
+
 class Expression : public Node
 {
 };
 
 class AssignmentExpression : public Expression
 {
-private:
-	Expression *lhs, *rhs;
-public:
-	AssignmentExpression(Expression* lhs, Expression* rhs)
-	{
-		this->lhs = lhs;
-		this->rhs = rhs;
-	}
+	private:
+		Expression *lhs, *rhs;
+	public:
+		AssignmentExpression(Expression* lhs, Expression* rhs)
+		{
+			this->lhs = lhs;
+			this->rhs = rhs;
+		}
+		virtual Expression* getLHS() { return lhs; }
+		virtual Expression* getRHS() { return rhs; }
+		virtual void printAssignExpression() {
+			cout << "Lefthandside expression is" << getLHS() << endl
+				 << "Righthandside expression is" << getRHS() << endl;
+		}
 };
 
 
 class IdentifierExpression : public Expression
 {
-	private :
+	private:
 		char *name;
 	public:
 		IdentifierExpression(char *name)
 		{
 			this->name = name;
+		}
+		virtual char getName() { return *name; }
+		virtual void printIndentifierExpression() {
+			cout << "Indentifier expression is" << getName() << endl;
 		}
 };
 
@@ -36,5 +49,11 @@ class IntegerLiteralExpression : public Expression
 		IntegerLiteralExpression(int value)
 		{
 			this->value = value;
+		}
+		virtual int getValue() {
+			return value;
+		}
+		virtual void printIntLiteralExpression() {
+			cout << "IntLiteral expression is" << getValue() << endl;
 		}
 };
